6759b3f553d5c9e513986c2640c69ea0137e5617
ActuallyTaylor <taylor.lineman@gmail.com>
2023-11-09 00:43:45 -0500

    Much better streaming
    Signed-off-by: ActuallyTaylor <taylor.lineman@gmail.com>

--- Sources/SLlama/Client.swift
+++ Sources/SLlama/Client.swift
@@ -7,37 +7,81 @@
 
 import Foundation
 
-public class Client: NSObject {
-    enum StreamStatus {
+public protocol ClientStreamDelegate {
+    func didReceiveModel<Model: Codable>(model: Model)
+    func didFinish(error: Error?)
+}
+
+public class ClientStream<Model: Codable>: NSObject, URLSessionDataDelegate {
+    public enum StreamStatus {
         case none
-        case preparing
         case started
-        case stopped
+        case suspended
+        case canceled
+        case finished
     }
     
-    struct StreamConfiguration {
-        let model: Codable.Type
-    }
-    
-    private let baseURLString: String
     private var session: URLSession! = nil
-
-    private var streamingTask: URLSessionDataTask? = nil
-    private var streamConfig: StreamConfiguration? = nil
+    private var task: URLSessionTask? = nil
     
-    var streamStatus: StreamStatus = .none
+    public var delegate: ClientStreamDelegate?
+    public var status: StreamStatus = .none
     
-
-    public init(baseURLString: String) {
-        self.baseURLString = baseURLString
+    init(request: URLRequest) {
         super.init()
-
-        let config = URLSessionConfiguration.default
-        config.requestCachePolicy = .reloadIgnoringLocalCacheData
-        self.session = URLSession(configuration: config, delegate: self, delegateQueue: .main)
+        
+        self.session = URLSession(configuration: .default, delegate: self, delegateQueue: .main)
+        self.task = self.session.dataTask(with: request)
     }
     
-    public func stream<Model>(_ request: Request<Model>) throws {
+    public func resumeStream() {
+        task?.resume()
+        status = .started
+    }
+    
+    public func cancelStream() {
+        task?.cancel()
+        status = .canceled
+    }
+    
+    public func suspendStream() {
+        task?.suspend()
+        status = .suspended
+    }
+    
+    public func urlSession(_ session: URLSession, dataTask: URLSessionDataTask, didReceive data: Data) {
+        guard var string = String(data: data, encoding: .utf8) else { return }
+        string = string.replacingOccurrences(of: "data: ", with: "")
+
+        guard let stringData = string.data(using: .utf8) else { return }
+
+        let model = try? Model.decode(data: stringData)
+        
+        delegate?.didReceiveModel(model: model)
+    }
+
+    public func urlSession(_ session: URLSession, task: URLSessionTask, didCompleteWithError error: Error?) {
+        if let error = error as NSError? {
+            print("Error: \(error)")
+        } else {
+            print("task complete")
+        }
+        
+        status = .finished
+        delegate?.didFinish(error: error)
+    }
+}
+
+public struct Client {
+    private let baseURLString: String
+    private var session: URLSession
+
+    public init(baseURLString: String, session: URLSession = .shared) {
+        self.baseURLString = baseURLString
+        self.session = session
+    }
+    
+    public func stream<Model>(_ request: Request<Model>) throws -> ClientStream<Model> {
         guard let requestComponents = URLComponents(baseURL: baseURLString, request: request) else {
             throw SLlamaClientError.invalidBaseURL
         }
@@ -46,16 +90,9 @@ public class Client: NSObject {
             throw SLlamaClientError.invalidBaseURL
         }
         
-        streamStatus = .preparing
         let urlRequest = try URLRequest(url: url, request: request)
-
-        let task = self.session.dataTask(with: urlRequest)
-
-        self.streamingTask = task
-        
-        streamConfig = .init(model: Model.self)
-        streamStatus = .started
-        task.resume()
+                
+        return ClientStream.init(request: urlRequest)
     }
     
     public func run<Model>(_ request: Request<Model>) async throws -> Model {
@@ -81,27 +118,4 @@ public class Client: NSObject {
             throw SLlamaClientError.invalidModel(error: error)
         }
     }
-}
-
-extension Client: URLSessionDataDelegate {
-    public func urlSession(_ session: URLSession, dataTask: URLSessionDataTask, didReceive data: Data) {
-        guard let streamConfig else { return }
-        guard var string = String(data: data, encoding: .utf8) else { return }
-        string = string.replacingOccurrences(of: "data: ", with: "")
-        
-        guard let stringData = string.data(using: .utf8) else { return }
-        
-        let model = try? streamConfig.model.decode(data: stringData)
-        print(model)
-    }
-
-    public func urlSession(_ session: URLSession, task: URLSessionTask, didCompleteWithError error: Error?) {
-        if let error = error as NSError? {
-            print("Error: \(error)")
-        } else {
-            print("task complete")
-        }
-        streamStatus = .stopped
-    }
-
 }
--- Tests/SLlamaTests/SLlamaTests.swift
+++ Tests/SLlamaTests/SLlamaTests.swift
@@ -14,18 +14,18 @@ final class SLlamaTests: XCTestCase {
 
     """
     
-//    func testCompletionEndpoint() async throws {
-//        let client = Client(baseURLString: "http://127.0.0.1:8080")
-//        
-//        let preparedPrompt = PromptProcessor.prepareTemplate(template: template, systemPrompt: "A chat between a curious human and an artificial intelligence assistant. The assistant gives helpful, detailed, and polite answers to the human's questions.", userPrompt: "Hello Assistant")
-//        let settings: CompletionSettings = .init(prompt: preparedPrompt, temperature: 0.7, n_predict: 256)
-//        
-//        let request = LlamaRequests.completion(settings)
-//        
-//        let response = try await client.run(request)
-//        
-//        print(response.content)
-//    }
+    func testCompletionEndpoint() async throws {
+        let client = Client(baseURLString: "http://127.0.0.1:8080")
+
+        let preparedPrompt = PromptProcessor.prepareTemplate(template: template, systemPrompt: "A chat between a curious human and an artificial intelligence assistant. The assistant gives helpful, detailed, and polite answers to the human's questions.", userPrompt: "Hello Assistant")
+        let settings: CompletionSettings = .init(prompt: preparedPrompt, temperature: 0.7, n_predict: 256)
+
+        let request = LlamaRequests.completion(settings)
+
+        let response = try await client.run(request)
+
+        print(response.content)
+    }
     
     func testCompletionStreaming() async throws {
         let client = Client(baseURLString: "http://127.0.0.1:8080")
@@ -35,10 +35,29 @@ final class SLlamaTests: XCTestCase {
         
         let request = LlamaRequests.completion(settings)
         
-        try client.stream(request)
+        let clientStream = try client.stream(request)
         
-        while client.streamStatus != .stopped {
-            
+        clientStream.delegate = self
+        
+        clientStream.resumeStream()
+        
+        while clientStream.status != .finished {
+
+        }
+    }
+}
+
+extension SLlamaTests: ClientStreamDelegate {
+    func didReceiveModel<Model>(model: Model) where Model : Codable {
+        guard let model = model as? CompletionResult else { return }
+        print(model.content, terminator: "")
+    }
+    
+    func didFinish(error: Error?) {
+        if let error {
+            print("Finished with error: \(error)")
+        } else {
+            print("Finished with no error")
         }
     }
 }
