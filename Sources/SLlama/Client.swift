1f8fd40ac0d8b46ebba2679d1040d43bd0e1639b
ActuallyTaylor <taylor.lineman@gmail.com>
2023-11-09 00:23:36 -0500

    Working streaming, very rough
    Signed-off-by: ActuallyTaylor <taylor.lineman@gmail.com>

--- Sources/SLlama/Client.swift
+++ Sources/SLlama/Client.swift
@@ -7,13 +7,55 @@
 
 import Foundation
 
-public struct Client {
-    private let baseURLString: String
-    private let session: URLSession
+public class Client: NSObject {
+    enum StreamStatus {
+        case none
+        case preparing
+        case started
+        case stopped
+    }
     
-    public init(baseURLString: String, session: URLSession = .shared) {
-        self.session = session
+    struct StreamConfiguration {
+        let model: Codable.Type
+    }
+    
+    private let baseURLString: String
+    private var session: URLSession! = nil
+
+    private var streamingTask: URLSessionDataTask? = nil
+    private var streamConfig: StreamConfiguration? = nil
+    
+    var streamStatus: StreamStatus = .none
+    
+
+    public init(baseURLString: String) {
         self.baseURLString = baseURLString
+        super.init()
+
+        let config = URLSessionConfiguration.default
+        config.requestCachePolicy = .reloadIgnoringLocalCacheData
+        self.session = URLSession(configuration: config, delegate: self, delegateQueue: .main)
+    }
+    
+    public func stream<Model>(_ request: Request<Model>) throws {
+        guard let requestComponents = URLComponents(baseURL: baseURLString, request: request) else {
+            throw SLlamaClientError.invalidBaseURL
+        }
+
+        guard let url = requestComponents.url else {
+            throw SLlamaClientError.invalidBaseURL
+        }
+        
+        streamStatus = .preparing
+        let urlRequest = try URLRequest(url: url, request: request)
+
+        let task = self.session.dataTask(with: urlRequest)
+
+        self.streamingTask = task
+        
+        streamConfig = .init(model: Model.self)
+        streamStatus = .started
+        task.resume()
     }
     
     public func run<Model>(_ request: Request<Model>) async throws -> Model {
@@ -39,4 +81,27 @@ public struct Client {
             throw SLlamaClientError.invalidModel(error: error)
         }
     }
+}
+
+extension Client: URLSessionDataDelegate {
+    public func urlSession(_ session: URLSession, dataTask: URLSessionDataTask, didReceive data: Data) {
+        guard let streamConfig else { return }
+        guard var string = String(data: data, encoding: .utf8) else { return }
+        string = string.replacingOccurrences(of: "data: ", with: "")
+        
+        guard let stringData = string.data(using: .utf8) else { return }
+        
+        let model = try? streamConfig.model.decode(data: stringData)
+        print(model)
+    }
+
+    public func urlSession(_ session: URLSession, task: URLSessionTask, didCompleteWithError error: Error?) {
+        if let error = error as NSError? {
+            print("Error: \(error)")
+        } else {
+            print("task complete")
+        }
+        streamStatus = .stopped
+    }
+
 }
--- Tests/SLlamaTests/SLlamaTests.swift
+++ Tests/SLlamaTests/SLlamaTests.swift
@@ -14,16 +14,31 @@ final class SLlamaTests: XCTestCase {
 
     """
     
-    func testCompletionEndpoint() async throws {
+//    func testCompletionEndpoint() async throws {
+//        let client = Client(baseURLString: "http://127.0.0.1:8080")
+//        
+//        let preparedPrompt = PromptProcessor.prepareTemplate(template: template, systemPrompt: "A chat between a curious human and an artificial intelligence assistant. The assistant gives helpful, detailed, and polite answers to the human's questions.", userPrompt: "Hello Assistant")
+//        let settings: CompletionSettings = .init(prompt: preparedPrompt, temperature: 0.7, n_predict: 256)
+//        
+//        let request = LlamaRequests.completion(settings)
+//        
+//        let response = try await client.run(request)
+//        
+//        print(response.content)
+//    }
+    
+    func testCompletionStreaming() async throws {
         let client = Client(baseURLString: "http://127.0.0.1:8080")
         
         let preparedPrompt = PromptProcessor.prepareTemplate(template: template, systemPrompt: "A chat between a curious human and an artificial intelligence assistant. The assistant gives helpful, detailed, and polite answers to the human's questions.", userPrompt: "Hello Assistant")
-        let settings: CompletionSettings = .init(prompt: preparedPrompt, temperature: 0.7, n_predict: 256)
+        let settings: CompletionSettings = .init(prompt: preparedPrompt, temperature: 0.7, n_predict: 256, stream: true)
         
         let request = LlamaRequests.completion(settings)
         
-        let response = try await client.run(request)
+        try client.stream(request)
         
-        print(response.content)
+        while client.streamStatus != .stopped {
+            
+        }
     }
 }
